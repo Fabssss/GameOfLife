b7e6db02bfc0c76df56982763a930b4b0c5facf8
Fabiha Makhdoomi <makhdoomifabiha@gmail.com>
2021-12-30 19:16:47 +0530

    Update GOL.java and create methods to print and update matrix

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 public class Main
 {
-    public public static void main(String[] args) {
-            
+    public static void main(String[] args) {
+
     }
+    public static int[][] matrixGenerator(){
+        int rows = 20;
+        int columns = 20;
+        int matrix[][] = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    return matrix;}  
 }
